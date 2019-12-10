f3e0601a3fc6488f492facd86ceaf56ae7a9f700
Pavel Semkin <pvsemk@mail.ru>
2019-12-10 21:32:50 +0300

    Done

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import time
 from copy import deepcopy
 import numpy as np
 from tabulate import tabulate
+from collections import deque
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--grammar',
                         type=str,
-                        default='grammar.txt',
+                        default='grammar_1.txt',
                         help='Path to file containing grammar'
                         )
     parser.add_argument('--word',
                         type=str,
-                        default='id+id*id$')
+                        default='id + id * id $') # 'a c b g f h' for grammar_1
 
     return parser.parse_args()
 
@@ -73,12 +74,10 @@ def find_follows(transitions, nonterminals, firsts, start_symbol, n_iter=5):
                     if nonterminal in production:
                         if nonterminal == production[-1]:
                             follows[nonterminal].update(follows[key])
-                        else:  # TODO: implement follow(B) for case aBc where c contains several nonterminals with eps
-                            next_symbol = production[production.index(nonterminal) + 1]
-                            tmp = deepcopy(firsts[next_symbol])
-                            tmp.discard('ep')
-                            follows[nonterminal].update(tmp)
-                            if 'ep' in firsts[next_symbol]:
+                        else:
+                            first_of_remain = find_first(production[production.index(nonterminal) + 1:], firsts, nonterminals)
+                            follows[nonterminal].update(first_of_remain.difference(['ep']))
+                            if 'ep' in first_of_remain:
                                 follows[nonterminal].update(follows[key])
     return follows
 
@@ -128,17 +127,43 @@ def build_table(grammar):
         firsts.pop(key)
     table = np.insert(table, 0, list(transitions.keys()), axis=1)
     terminals.append('$')
-    terminals.insert(0, None)
-    table = np.insert(table, 0, terminals, axis=0)
-    return firsts, follows, table, terminals,
+    tmp = deepcopy(terminals)
+    tmp.insert(0, None)
+    table = np.insert(table, 0, tmp, axis=0)
+    return firsts, follows, table, terminals, transitions
+
+
+def parse_word(word, table, start_symbol, terminals, nonterminals):
+    print('Processing string {} with table'.format(word))
+    stack = deque((start_symbol, '$'))
+    input_stream = deque(word.split())
+    stack_symbol = start_symbol
+    while stack_symbol != '$':
+        input_symbol = input_stream[0]
+        if stack_symbol == input_symbol:
+            stack.popleft()
+            input_stream.popleft()
+            stack_symbol = stack[0]
+            continue
+        elif stack_symbol in terminals:
+            raise RuntimeError('Unable to process the string with the grammar')
+        row = nonterminals.index(stack_symbol)
+        col = terminals.index(input_symbol)
+        if table[row, col] is None:
+            raise RuntimeError('Unable to process the string with the grammar')
+        else:
+            print('\tProduction {} -> {}'.format(stack_symbol, table[row, col]))
+            stack.popleft()
+            if table[row, col] != 'ep':
+                stack.extendleft(reversed(table[row, col].split()))
+        stack_symbol = stack[0]
+
 
 
 def main():
     args = parse_args()
     grammar = read_grammar(args.grammar)
-    firsts, follows, table, terminals = build_table(grammar)
-    terminals.insert(0, None)
-    terminals.append('$')
+    firsts, follows, table, terminals, transitions = build_table(grammar)
     print('Firsts: ')
     print_dict(firsts)
     print('Follows:')
@@ -146,6 +171,7 @@ def main():
     print('Table:')
     print(tabulate(table, tablefmt='fancy_grid'))
     table = table[1:, 1:]
+    parse_word(args.word, table, grammar[0][0], terminals, list(transitions.keys()))
 
 
 
